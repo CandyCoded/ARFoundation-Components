bdb55e52a88d09ccbd5112757e0706a099f78bff
Scott Doxey <neogeek0101@gmail.com>
2018-09-21 18:17:23 -0400

    Added ARDistanceFromPlane script.

---
+++ Assets/Plugin/ARFoundation-Components/Scripts/ARDistanceFromPlane.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+namespace CandyCoded
+{
+
+    [RequireComponent(typeof(ARSessionOrigin))]
+    [RequireComponent(typeof(ARPlaneManager))]
+    public class ARDistanceFromPlane : MonoBehaviour
+    {
+
+        [SerializeField]
+        [EnumMask]
+        private PlaneAlignment planeAlignment = PlaneAlignment.Horizontal;
+
+        public delegate void DistanceEvent(float distance);
+        public event DistanceEvent DistanceUpdate;
+
+        [HideInInspector]
+        public ARSessionOrigin sessionOrigin;
+
+        [HideInInspector]
+        public ARPlaneManager planeManager;
+
+        private void Awake()
+        {
+
+            sessionOrigin = gameObject.GetComponent<ARSessionOrigin>();
+            planeManager = gameObject.GetComponent<ARPlaneManager>();
+
+        }
+
+        private void Update()
+        {
+
+            if (planeManager.enabled && DistanceUpdate != null)
+            {
+
+                Pose pose;
+
+                bool planeVisible = ARFoundationExtensions.IsLookingAtPlane(sessionOrigin, planeManager, planeAlignment, out pose);
+
+                Vector3 distanceFromPlane = sessionOrigin.camera.transform.position - pose.position;
+
+                DistanceUpdate(Mathf.Abs(distanceFromPlane.magnitude));
+
+            }
+
+        }
+
+    }
+
+}
