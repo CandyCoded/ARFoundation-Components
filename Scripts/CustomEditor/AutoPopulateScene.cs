167099e82ce9bf821173724c24d948b0e7f35c8b
Scott Doxey <neogeek0101@gmail.com>
2019-03-12 22:57:00 -0400

    Prevent errors when re-running script.

--- Scripts/CustomEditor/AutoPopulateScene.cs
+++ Scripts/CustomEditor/AutoPopulateScene.cs
@@ -3,6 +3,7 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.XR.ARFoundation;
+using UnityEditor.Events;
 
 namespace CandyCoded.ARFoundationComponents
 {
@@ -24,21 +25,47 @@ namespace CandyCoded.ARFoundationComponents
         private static void SetupARFoundationGameObjects()
         {
 
-            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session Origin");
+            if (!GameObject.Find("AR Session Origin"))
+            {
 
-            Object.DestroyImmediate(GameObject.Find("Main Camera"));
+                EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session Origin");
 
-            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session");
+            }
 
-            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Default Plane");
+            var mainCamera = GameObject.Find("Main Camera");
 
-            var defaultPlanePrefab = PrefabUtility.SaveAsPrefabAsset(GameObject.Find("AR Default Plane"), "Assets/AR Default Plane.prefab");
+            if (mainCamera)
+            {
 
-            Object.DestroyImmediate(GameObject.Find("AR Default Plane"));
+                Object.DestroyImmediate(mainCamera);
+
+            }
+
+            if (!GameObject.Find("AR Session"))
+            {
+
+                EditorApplication.ExecuteMenuItem("GameObject/XR/AR Session");
+
+            }
+
+            var defaultPlanePrefabPath = "Assets/AR Default Plane.prefab";
+
+            var defaultPlanePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(defaultPlanePrefabPath);
+
+            if (!defaultPlanePrefab)
+            {
+
+                EditorApplication.ExecuteMenuItem("GameObject/XR/AR Default Plane");
+
+                defaultPlanePrefab = PrefabUtility.SaveAsPrefabAsset(GameObject.Find("AR Default Plane"), defaultPlanePrefabPath);
+
+                Object.DestroyImmediate(GameObject.Find("AR Default Plane"));
+
+            }
 
             var sessionOrigin = GameObject.Find("AR Session Origin");
 
-            var planeManager = sessionOrigin.AddComponent<ARPlaneManager>();
+            var planeManager = sessionOrigin.AddOrGetComponent<ARPlaneManager>();
 
             planeManager.planePrefab = defaultPlanePrefab;
 
@@ -52,26 +79,26 @@ namespace CandyCoded.ARFoundationComponents
             var planeEvents = sessionOrigin.AddOrGetComponent<ARPlaneEvents>();
             var placeObjectsOnPlane = sessionOrigin.AddOrGetComponent<ARPlaceObjectOnPlane>();
 
-            //UnityEventTools.AddPersistentListener(planeEvents.PlaneTouchedWithTouchPosition, placeObjectsOnPlane.PlaceObjectOnPlane);
+            UnityEventTools.AddPersistentListener(planeEvents.PlaneTouchedWithTouchPosition, placeObjectsOnPlane.PlaceObjectOnPlane);
 
-            var cubePrefab = CreateDemoCubePrefab();
+            var cubePrefabPath = "Assets/Cube.prefab";
+
+            var cubePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+
+            if (!cubePrefab)
+            {
+
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = Vector3.one * 0.1f;
+
+                cubePrefab = PrefabUtility.SaveAsPrefabAsset(cube, "Assets/Cube.prefab");
+
+                Object.DestroyImmediate(cube);
+
+            }
 
             placeObjectsOnPlane.objectToPlace = cubePrefab;
             placeObjectsOnPlane.placeMultiple = true;
-
-        }
-
-        private static GameObject CreateDemoCubePrefab()
-        {
-
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.localScale = Vector3.one * 0.1f;
-
-            var cubePrefabs = PrefabUtility.SaveAsPrefabAsset(cube, "Assets/Cube.prefab");
-
-            Object.DestroyImmediate(cube);
-
-            return cubePrefabs;
 
         }
 
