fbc19ca1c2f3c874664065684cccbeb47d768c36
Scott Doxey <neogeek0101@gmail.com>
2019-06-05 18:00:15 -0400

    Added new auto populate scene functionality for image and object detection.

--- Scripts/CustomEditor/AutoPopulateScene.cs
+++ Scripts/CustomEditor/AutoPopulateScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.XR.ARFoundation;
 using UnityEditor.Events;
+using UnityEngine.XR.ARSubsystems;
 
 namespace CandyCoded.ARFoundationComponents
 {
@@ -11,18 +12,55 @@ namespace CandyCoded.ARFoundationComponents
     public static class AutoPopulateScene
     {
 
-        [MenuItem("Window/CandyCoded/ARFoundation Components/Quick Setup")]
-        private static void PopulateSceneWithARGameObjects()
+        private const string defaultPlanePrefabPath = "Assets/AR Default Plane.prefab";
+
+        private const string cubePrefabPath = "Assets/Cube.prefab";
+
+        private const string imageReferenceLibraryPath = "Assets/ReferenceImageLibrary.asset";
+
+        private const string objectReferenceLibraryPath = "Assets/ReferenceObjectLibrary.asset";
+
+        [MenuItem("Window/CandyCoded/ARFoundation Components/Quick Setup/Plane Detection")]
+        private static void QuickSetupPlaneDetection()
         {
 
             EditorApplication.isPlaying = false;
 
-            SetupARFoundationGameObjects();
-            SetupARFoundationComponentsGameObjects();
+            SetupARFoundation();
+            SetupARFoundationComponents();
+            SetupARFoundationComponentsDemoPlane();
+            SetupARFoundationComponentsDemoCube();
+            SetupARFoundationComponentsPlaneDetection();
 
         }
 
-        private static void SetupARFoundationGameObjects()
+        [MenuItem("Window/CandyCoded/ARFoundation Components/Quick Setup/Image Detection")]
+        private static void QuickSetupImageDetection()
+        {
+
+            EditorApplication.isPlaying = false;
+
+            SetupARFoundation();
+            SetupARFoundationComponents();
+            SetupARFoundationComponentsDemoCube();
+            SetupARFoundationComponentsImageDetection();
+
+        }
+
+        [MenuItem("Window/CandyCoded/ARFoundation Components/Quick Setup/Object Detection")]
+        private static void QuickSetupObjectDetection()
+        {
+
+            EditorApplication.isPlaying = false;
+
+            SetupARFoundation();
+            SetupARFoundationComponents();
+            SetupARFoundationComponentsDemoCube();
+            SetupARFoundationComponentsObjectDetection();
+
+        }
+
+        private static void SetupARFoundation()
         {
 
             if (!GameObject.Find("AR Session Origin"))
@@ -48,35 +86,14 @@ namespace CandyCoded.ARFoundationComponents
 
             }
 
-            const string defaultPlanePrefabPath = "Assets/AR Default Plane.prefab";
-
-            var defaultPlanePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(defaultPlanePrefabPath);
-
-            if (!defaultPlanePrefab)
-            {
-
-                EditorApplication.ExecuteMenuItem("GameObject/XR/AR Default Plane");
-
-                defaultPlanePrefab = PrefabUtility.SaveAsPrefabAsset(GameObject.Find("AR Default Plane"), defaultPlanePrefabPath);
-
-                Object.DestroyImmediate(GameObject.Find("AR Default Plane"));
-
-            }
-
             var sessionOrigin = GameObject.Find("AR Session Origin");
 
             sessionOrigin.AddOrGetComponent<ARRaycastManager>();
 
-            var planeManager = sessionOrigin.AddOrGetComponent<ARPlaneManager>();
-
-            planeManager.planePrefab = defaultPlanePrefab;
-
         }
 
-        private static void SetupARFoundationComponentsGameObjects()
+        private static void SetupARFoundationComponents()
         {
-
-            var sessionOrigin = GameObject.Find("AR Session Origin");
 
             var camera = GameObject.Find("AR Camera");
             var cameraManager = camera.AddOrGetComponent<ARCameraManager>();
@@ -85,6 +102,54 @@ namespace CandyCoded.ARFoundationComponents
             var lightEstimation = light.AddOrGetComponent<ARLightEstimation>();
             lightEstimation.cameraManager = cameraManager;
 
+        }
+
+        private static void SetupARFoundationComponentsDemoPlane()
+        {
+
+            var defaultPlanePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(defaultPlanePrefabPath);
+
+            if (defaultPlanePrefab)
+            {
+                return;
+            }
+
+            EditorApplication.ExecuteMenuItem("GameObject/XR/AR Default Plane");
+
+            PrefabUtility.SaveAsPrefabAsset(GameObject.Find("AR Default Plane"), defaultPlanePrefabPath);
+
+            Object.DestroyImmediate(GameObject.Find("AR Default Plane"));
+
+        }
+
+        private static void SetupARFoundationComponentsDemoCube()
+        {
+
+            var cubePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+
+            if (cubePrefab)
+            {
+                return;
+            }
+
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.localScale = Vector3.one * 0.1f;
+
+            PrefabUtility.SaveAsPrefabAsset(cube, cubePrefabPath);
+
+            Object.DestroyImmediate(cube);
+
+        }
+
+        private static void SetupARFoundationComponentsPlaneDetection()
+        {
+
+            var sessionOrigin = GameObject.Find("AR Session Origin");
+
+            var planeManager = sessionOrigin.AddOrGetComponent<ARPlaneManager>();
+
+            planeManager.planePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(defaultPlanePrefabPath);
+
             var planeEvents = sessionOrigin.AddOrGetComponent<ARPlaneEvents>();
             var placeObjectsOnPlane = sessionOrigin.AddOrGetComponent<ARPlaceObjectOnPlane>();
 
@@ -92,24 +157,54 @@ namespace CandyCoded.ARFoundationComponents
 
             UnityEventTools.AddPersistentListener(planeEvents.PlaneTouchedWithTouchPosition, placeObjectsOnPlane.PlaceObjectOnPlane);
 
-            const string cubePrefabPath = "Assets/Cube.prefab";
+            placeObjectsOnPlane.objectToPlace = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+            placeObjectsOnPlane.placeMultiple = true;
 
-            var cubePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+        }
 
-            if (!cubePrefab)
+        private static void SetupARFoundationComponentsImageDetection()
+        {
+
+            var sessionOrigin = GameObject.Find("AR Session Origin");
+            var trackedImageManager = sessionOrigin.AddOrGetComponent<ARTrackedImageManager>();
+
+            trackedImageManager.trackedImagePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+
+            var referenceLibrary = AssetDatabase.LoadAssetAtPath<XRReferenceImageLibrary>(imageReferenceLibraryPath);
+
+            if (!referenceLibrary)
             {
 
-                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.localScale = Vector3.one * 0.1f;
+                referenceLibrary = ScriptableObject.CreateInstance<XRReferenceImageLibrary>();
 
-                cubePrefab = PrefabUtility.SaveAsPrefabAsset(cube, "Assets/Cube.prefab");
-
-                Object.DestroyImmediate(cube);
+                AssetDatabase.CreateAsset(referenceLibrary, imageReferenceLibraryPath);
 
             }
 
-            placeObjectsOnPlane.objectToPlace = cubePrefab;
-            placeObjectsOnPlane.placeMultiple = true;
+            trackedImageManager.referenceLibrary = AssetDatabase.LoadAssetAtPath<XRReferenceImageLibrary>(imageReferenceLibraryPath);
+
+        }
+
+        private static void SetupARFoundationComponentsObjectDetection()
+        {
+
+            var sessionOrigin = GameObject.Find("AR Session Origin");
+            var trackedObjectManager = sessionOrigin.AddOrGetComponent<ARTrackedObjectManager>();
+
+            trackedObjectManager.trackedObjectPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cubePrefabPath);
+
+            var referenceLibrary = AssetDatabase.LoadAssetAtPath<XRReferenceObjectLibrary>(objectReferenceLibraryPath);
+
+            if (!referenceLibrary)
+            {
+
+                referenceLibrary = ScriptableObject.CreateInstance<XRReferenceObjectLibrary>();
+
+                AssetDatabase.CreateAsset(referenceLibrary, objectReferenceLibraryPath);
+
+            }
+
+            trackedObjectManager.referenceLibrary = referenceLibrary;
 
         }
 
