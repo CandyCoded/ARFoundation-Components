f8a96a4565f0f158e30aadda9e00d7cda31aedfe
neogeek <neogeek0101@gmail.com>
2019-08-25 08:59:32 -0400

    Added missing script header comments.

--- Assets/Plugins/ARFoundation-Components/Scripts/ARFoundationExtensions.cs
+++ Assets/Plugins/ARFoundation-Components/Scripts/ARFoundationExtensions.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
