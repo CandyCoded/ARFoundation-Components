19985e24b0b5de0367cb96343a397257eccf4458
neogeek <neogeek0101@gmail.com>
2019-09-02 19:03:53 -0400

    Switched to CandyCoded editor methods for creating assets.

--- Scripts/CustomEditor/QuickSetupImageDetection.cs
+++ Scripts/CustomEditor/QuickSetupImageDetection.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #if UNITY_EDITOR
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -12,7 +13,11 @@ namespace CandyCoded.ARFoundationComponents.Editor
     public static class QuickSetupImageDetection
     {
 
-        private const string imageReferenceLibraryPath = "Assets/ReferenceImageLibrary.asset";
+        private const string sessionOriginName = "AR Session Origin";
+
+        private const string sessionOriginMenuPath = "GameObject/XR/AR Session Origin";
+
+        private const string imageReferenceLibraryAssetPath = "Assets/ReferenceImageLibrary.asset";
 
         [MenuItem("Window/CandyCoded/ARFoundation Components/Quick Setup/Image Detection")]
         private static void Setup()
@@ -24,31 +29,47 @@ namespace CandyCoded.ARFoundationComponents.Editor
             AutoPopulateScene.SetupARFoundationComponents();
             AutoPopulateScene.SetupARFoundationComponentsDemoCube();
 
+            SetupARFoundationImageDetection();
             SetupARFoundationComponentsImageDetection();
+
+        }
+
+        private static void SetupARFoundationImageDetection()
+        {
+
+            var sessionOrigin =
+                CustomEditorExtensions.FindOrCreateGameObjectFromAssetMenu(sessionOriginName, sessionOriginMenuPath);
+
+            var trackedImageManager = sessionOrigin.AddOrGetComponent<ARTrackedImageManager>();
+
+            trackedImageManager.trackedImagePrefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>(AutoPopulateScene.cubePrefabPath);
+
+            var referenceLibrary =
+                CustomEditorExtensions.FindOrCreateScriptableObjectAtPath<XRReferenceImageLibrary>(
+                    imageReferenceLibraryAssetPath);
+
+            try
+            {
+
+                trackedImageManager.referenceLibrary = referenceLibrary;
+
+            }
+            catch (Exception err)
+            {
+
+                Debug.LogWarning(err);
+
+            }
 
         }
 
         private static void SetupARFoundationComponentsImageDetection()
         {
 
-            var sessionOrigin = GameObject.Find("AR Session Origin");
-            var trackedImageManager = sessionOrigin.AddOrGetComponent<ARTrackedImageManager>();
+            var sessionOrigin = GameObject.Find(sessionOriginName);
 
-            trackedImageManager.trackedImagePrefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>(AutoPopulateScene.cubePrefabPath);
-
-            var referenceLibrary = AssetDatabase.LoadAssetAtPath<XRReferenceImageLibrary>(imageReferenceLibraryPath);
-
-            if (!referenceLibrary)
-            {
-
-                referenceLibrary = ScriptableObject.CreateInstance<XRReferenceImageLibrary>();
-
-                AssetDatabase.CreateAsset(referenceLibrary, imageReferenceLibraryPath);
-
-            }
-
-            trackedImageManager.referenceLibrary = referenceLibrary;
+            sessionOrigin.AddOrGetComponent<ARTrackedImageEvents>();
 
         }
 
